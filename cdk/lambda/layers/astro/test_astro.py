ba0a75f8b1a570bdcf247ebb5e5eaa7ba31b5cda
Hadrian Hughes <hadrian.hughes@and.digital>
2020-11-12 10:34:55 +0000

    Add unit test for get_all_objects function

--- cdk/lambda/layers/astro/test_astro.py
+++ cdk/lambda/layers/astro/test_astro.py
@@ -1,5 +1,5 @@
 import math
-from astro.astro import get_object
+from astro.astro import get_object, get_all_objects
 
 expected_greenwich_2000 = [
     ('mercury', { 'alt': -62, 'az': 17 }),
@@ -11,8 +11,23 @@ expected_greenwich_2000 = [
     ('neptune', { 'alt': -52, 'az': 318 })
 ]
 
+greenwich_lat_long = (51.4934, 0.0098)
+datetime_2000 = '2000-01-01 00:00'
+
 def test_get_object():
     for (objName, position) in expected_greenwich_2000:
-        result = get_object(objName, (51.4934, 0.0098), '2000-01-01 00:00')
+        result = get_object(objName, greenwich_lat_long, datetime_2000)
         assert math.modf(result['alt'])[1] == position['alt']
         assert math.modf(result['az'])[1] == position['az']
+
+
+def test_get_all_objects():
+    result = get_all_objects(greenwich_lat_long, datetime_2000)
+    expected_dict = { objName: position
+                      for (objName, position)
+                      in expected_greenwich_2000 }
+
+    for item in result:
+        expected_item = expected_dict[item['object']]
+        assert math.modf(item['alt'])[1] == expected_item['alt']
+        assert math.modf(item['az'])[1] == expected_item['az']
