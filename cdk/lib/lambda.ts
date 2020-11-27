4c0e27a70b1f851a0970028d0046bd8e213bca8f
Hadrian Hughes <hadrian.hughes@and.digital>
2020-11-27 15:50:15 +0000

    Incorporate new Geo layer into Lambda library

--- cdk/lib/lambda.ts
+++ cdk/lib/lambda.ts
@@ -14,13 +14,14 @@ export class PlanetsLambdaLibrary extends cdk.Construct {
     // Create layers
     this.layers.astro = this.makeLayer('AstroLayer', 'A Python layer containing the core logic for the API', 'astro')
     this.layers.common = this.makeLayer('CommonLayer', 'A Python layer containing utility functions to be used across the project', 'common')
+    this.layers.geo = this.makeLayer('GeoLayer', 'A Python layer containing the logic for geographical positioning', 'geo')
 
 
     // Create functions
     this.functions.getVisibleByID = this.makeFunction(
       'GetVisibleByID',
       'getVisibleByID',
-      [this.layers.astro, this.layers.common],
+      [this.layers.astro, this.layers.common, this.layers.geo],
       {
         timeout: Duration.seconds(10),
         memorySize: 2048
