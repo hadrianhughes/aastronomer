b99014a681109cd70dbb6d0b4a365c05d8b8ac61
Hadrian Hughes <hadrian.hughes@and.digital>
2020-11-27 15:31:41 +0000

    Create function to derive lat and long from location ID

--- cdk/lambda/layers/geo/geo.py
+++ cdk/lambda/layers/geo/geo.py
@@ -1,6 +1,8 @@
 import math
 
 EARTH_RADIUS = 6371 #km
+ZONE_SIZE = 10 #km
+VERTICAL_ZONE_COUNT = 7
 
 # Haversine Formula
 def distance_between_points(lat1: float, lat2: float, lon1: float, lon2: float):
@@ -17,3 +19,20 @@ def distance_between_points(lat1: float, lat2: float, lon1: float, lon2: float):
 def lat_long_from_id(location_id: str):
     id_parts = location_id.split('-')
     [lat, long, zone_x, zone_y] = [int(x) for x in id_parts]
+
+    normalised_lat = 90 - lat
+    normalised_long = long if long <= 180 else -360 + long
+
+    if zone_y > VERTICAL_ZONE_COUNT - 1:
+        return None
+
+    region_width = distance_between_points(normalised_lat, normalised_lat, normalised_long, normalised_long + 1)
+    zone_count = math.ceil(region_width / ZONE_SIZE)
+
+    if zone_x > zone_count - 1:
+        return None
+
+    zone_height_degrees = 1 / VERTICAL_ZONE_COUNT
+    zone_width_degrees = 1 / zone_count
+
+    return (normalised_lat + (zone_height_degrees * zone_y) + (zone_height_degrees / 2), normalised_long + (zone_width_degrees * zone_x) + (zone_width_degrees / 2))
---
+++ cdk/lambda/layers/geo/tests/test_lat_long_from_id.py
@@ -0,0 +1,17 @@
+from geo import lat_long_from_id
+
+def test_out_of_bounds():
+    assert lat_long_from_id('0-0-0-7') == None
+    assert lat_long_from_id('0-0-12-0') == None
+
+
+def test_equator():
+    (lat, long) = lat_long_from_id('90-0-0-0')
+    assert round(lat, 3) == 0.071
+    assert round(long, 3) == 0.042
+
+
+def test_edinburgh():
+    (lat, long) = lat_long_from_id('35-356-5-6')
+    assert round(lat, 1) == 55.9
+    assert round(long, 1) == -3.2
