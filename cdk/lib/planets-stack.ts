481430bb5f255918a722fc7f046b5e19133aa8ce
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-02 16:04:19 +0000

    Remove unnecessary imports from PlanetsStack and add comments

--- cdk/lib/planets-stack.ts
+++ cdk/lib/planets-stack.ts
@@ -1,10 +1,6 @@
-import * as path from 'path'
 import * as cdk from '@aws-cdk/core';
 import * as cf from '@aws-cdk/aws-cloudfront'
-import * as lambda from '@aws-cdk/aws-lambda'
-import * as cr from '@aws-cdk/custom-resources'
-import * as iam from '@aws-cdk/aws-iam'
-import { AllowedMethods } from '@aws-cdk/aws-cloudfront'
+import { CloudFrontAllowedMethods } from '@aws-cdk/aws-cloudfront'
 import { HttpOrigin } from '@aws-cdk/aws-cloudfront-origins'
 import { Tags } from '@aws-cdk/core'
 import { PlanetsAPI } from './api'
@@ -21,11 +17,14 @@ export class PlanetsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: PlanetsStackProps) {
     super(scope, id, props);
 
+    // Initialise Lambda assets
     const planetsLambdas = new PlanetsLambdaLibrary(this, 'PlanetsLambdas')
 
+    // Set up API Gateway
     const api = new PlanetsAPI(this, 'PlanetsAPI', { lambdaLibrary: planetsLambdas })
     Tags.of(api).add('Module', 'API')
 
+    // Set up Lambda@Edge functions from EdgeStack us-east-1
     const edgeHandler = new EdgeHandler(this, 'EdgeHandler', {
       stack: this,
       parameterName: props.LAMBDA_EDGE_PARAMETER_NAME
@@ -40,10 +39,12 @@ export class PlanetsStack extends cdk.Stack {
           behaviors: [
             {
               pathPattern: '/visible/*',
+              allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
               isDefaultBehavior: true
             },
             {
               pathPattern: '/test_edge',
+              allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
               lambdaFunctionAssociations: [
                 {
                   eventType: cf.LambdaEdgeEventType.VIEWER_REQUEST,
@@ -55,6 +56,7 @@ export class PlanetsStack extends cdk.Stack {
         }
       ]
     })
+
 
     // Generate outputs
     new cdk.CfnOutput(this, 'APIEndpoint', {
