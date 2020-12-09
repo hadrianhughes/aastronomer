de5ed803cdaab4db1f75c33c9126cd9e2760670b
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-09 13:30:02 +0000

    Create querying my post code functionality

--- cdk/lambdas/edge/query_to_id/index.py
+++ cdk/lambdas/edge/query_to_id/index.py
@@ -1,5 +1,6 @@
 from urllib.parse import parse_qs, urlencode
 from geo import id_from_lat_long
+from geopy.geocoders import Nominatim
 
 possible_paths = {'/visible'}
 
@@ -22,12 +23,21 @@ def handler(event: dict, context: dict) -> dict:
 
         del query_params['lat']
         del query_params['long']
-        request['querystring'] = urlencode(query_params)
     elif 'postcode' in query_params:
-        # get lat/lon
-        # convert to id
-        return
+        postcode = query_params['postcode']
+
+        geolocator = Nominatim(user_agent='PlanetsAPI')
+        location = geolocator.geocode(postcode)
+
+        if location == None:
+            return { 'statusCode': 404, 'message': 'The postcode ' + postcode + ' could not be found.' }
+
+        lat_long_id = id_from_lat_long(location.latitude, location.longitude)
+        request['uri'] = request['uri'] + '/' + lat_long_id
+
+        del query_params['postcode']
     else:
         return { 'statusCode': 400, 'message': 'You must provide a lat and lon or a postcode.' }
 
+    request['querystring'] = urlencode(query_params)
     return request
--- cdk/lambdas/edge/query_to_id/test_query_to_id.py
+++ cdk/lambdas/edge/query_to_id/test_query_to_id.py
@@ -2,14 +2,14 @@ import json
 from urllib.parse import urlencode
 from .index import handler
 
-def make_request(uri: str, lat: float, long: float) -> dict:
+def make_request(uri: str, queries: dict) -> dict:
     return {
         'Records': [
             {
                 'cf': {
                     'request': {
                         'uri': uri,
-                        'querystring': urlencode({ 'lat': lat, 'long': long, 'foo': 'bar' })
+                        'querystring': urlencode({ **queries, 'foo': 'bar' })
                     }
                 }
             }
@@ -18,22 +18,27 @@ def make_request(uri: str, lat: float, long: float) -> dict:
 
 
 def test_bad_uri():
-    response = handler(make_request('/nothing', 0, 0), {})
+    response = handler(make_request('/nothing', { 'lat': 0, 'long': 0 }), {})
     assert response['statusCode'] == 404
 
 
 def test_success():
-    response = handler(make_request('/visible', 0, 0), {})
+    response = handler(make_request('/visible', { 'lat': 0, 'long': 0 }), {})
     assert response['uri'] == '/visible/90-0-0-0'
     assert response['querystring'] == 'foo=bar'
 
 
 def test_edinburgh():
-    decorated_request = handler(make_request('/visible', 55.9, -3.2), {})
+    decorated_request = handler(make_request('/visible', { 'lat': 55.9, 'long': -3.2 }), {})
     assert decorated_request['uri'] == '/visible/35-356-5-6'
 
 
 def test_api_prefix():
-    response = handler(make_request('/api/visible', 0, 0), {})
+    response = handler(make_request('/api/visible', { 'lat': 0, 'long': 0 }), {})
     assert response['uri'] == '/api/visible/90-0-0-0'
     assert response['querystring'] == 'foo=bar'
+
+
+def test_postcode():
+    response = handler(make_request('/visible', { 'postcode': 'EH1 2NG' }), {})
+    assert response['uri'] == '/visible/35-356-5-6'
