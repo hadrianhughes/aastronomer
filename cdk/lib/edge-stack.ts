8975245c51e660e118307cfa85b17d7fc37b282c
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-09 11:48:04 +0000

    Create one shared IAM Role for Edge functions in EdgeStack

--- cdk/lib/edge-stack.ts
+++ cdk/lib/edge-stack.ts
@@ -6,11 +6,22 @@ import * as ssm from '@aws-cdk/aws-ssm'
 import { PYTHON_RUNTIME, Dict } from './globals'
 
 export class EdgeStack extends cdk.Stack {
+  private customIamRole: iam.Role
+
   constructor(scope: cdk.Construct, id: string) {
     super(scope, id, {
       env: {
         region: 'us-east-1'
       }
+    })
+
+    // Create role for Lambda@Edge functions
+    this.customIamRole = new iam.Role(this, 'AllowLambdaServiceToAssumeRole', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('lambda.amazonaws.com'),
+        new iam.ServicePrincipal('edgelambda.amazonaws.com')
+      ),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')]
     })
 
     // Make Lambda@Edge functions
@@ -19,6 +30,7 @@ export class EdgeStack extends cdk.Stack {
       'StripAPIPath': this.makeEdgeFunction('StripAPIPath', 'strip_api_path')
     }
 
+    // Export an SSM Parameter for each function
     Object.keys(edgeFunctions).forEach(key => new ssm.StringParameter(this, `${key}ARN`, {
       parameterName: `/PlanetsAPI/${key}ARN`,
       description: `CDK parameter from ${key} Lambda@Edge function`,
@@ -31,13 +43,7 @@ export class EdgeStack extends cdk.Stack {
       runtime: PYTHON_RUNTIME,
       entry: path.join(__dirname, '..', 'lambdas', 'edge', dir),
       handler: 'handler',
-      role: new iam.Role(this, 'AllowLambdaServiceToAssumeRole', {
-        assumedBy: new iam.CompositePrincipal(
-          new iam.ServicePrincipal('lambda.amazonaws.com'),
-          new iam.ServicePrincipal('edgelambda.amazonaws.com')
-        ),
-        managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')]
-      })
+      role: this.customIamRole
     }).currentVersion.functionArn
   }
 }
