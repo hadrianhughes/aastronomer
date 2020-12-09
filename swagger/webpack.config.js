02a35ecf2b625785b343f7ca3c33bda3502fafa3
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-09 17:24:15 +0000

    Setup and add config for Swagger UI

---
+++ swagger/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './index',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'static' }
+    ])
+  ]
+}
