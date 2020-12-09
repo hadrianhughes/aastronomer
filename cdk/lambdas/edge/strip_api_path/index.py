5808937314c1735f5e366af939f443ff7e5677de
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-09 11:07:56 +0000

    Create lambda function to strip /api from paths

---
+++ cdk/lambdas/edge/strip_api_path/index.py
@@ -0,0 +1,7 @@
+def handler(event: dict, context: dict):
+    request = event['Records'][0]['cf']['request']
+    uri = request['uri']
+
+    request['uri'] = uri.replace('/api', '', 1)
+
+    return request
---
+++ cdk/lambdas/edge/strip_api_path/test_strip_api_path.py
@@ -0,0 +1,29 @@
+from .index import handler
+
+def make_request(uri: str):
+    return {
+        'Records': [
+            {
+                'cf': {
+                    'request': {
+                        'uri': uri
+                    }
+                }
+            }
+        ]
+    }
+
+
+def test_success():
+    stripped = handler(make_request('/api/test-path'), {})
+    assert stripped['uri'] == '/test-path'
+
+
+def test_not_api():
+    noop = handler(make_request('/test-path'), {})
+    assert noop['uri'] == '/test-path'
+
+
+def test_multiple_api():
+    stripped = handler(make_request('/api/api/test-path'), {})
+    assert stripped['uri'] == '/api/test-path'
