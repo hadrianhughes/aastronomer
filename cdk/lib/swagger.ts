2fa58f74bd4669da3cb3984b567989dc36cb8aa5
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-10 10:56:33 +0000

    Add bucket versioning to Swagger S3 bucket

--- cdk/lib/swagger.ts
+++ cdk/lib/swagger.ts
@@ -10,7 +10,8 @@ export class Swagger extends cdk.Construct {
     super(scope, id)
 
     this.bucket = new s3.Bucket(this, 'SwaggerBucket', {
-      websiteIndexDocument: 'index.html'
+      websiteIndexDocument: 'index.html',
+      versioned: true
     })
 
     new s3Deploy.BucketDeployment(this, 'DeploySwaggerBucket', {
